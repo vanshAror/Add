ad407841e200750efbc0973067dbc26b6db80991
vansh <avansh2001@gmail.com>
2024-03-11 12:45:23 +0530

    Initial commit

---
+++ Add/Add.h
@@ -0,0 +1,18 @@
+//
+//  Add.h
+//  Add
+//
+//  Created by Mobile on 11/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Add.
+FOUNDATION_EXPORT double AddVersionNumber;
+
+//! Project version string for Add.
+FOUNDATION_EXPORT const unsigned char AddVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Add/PublicHeader.h>
+
+
---
+++ Sum.swift
@@ -0,0 +1,12 @@
+//
+//  Sum.swift
+//  Add
+//
+//  Created by Mobile on 11/03/24.
+//
+
+import Foundation
+
+public func sum(a:Int,b:Int) -> Int {
+    return a + b
+}
